073024499682d271be3acf814d91dcf3d0a69c71
Dan <danieltongnz@gmail.com>
2017-10-25 10:40:25 +1300

    Initial commit
    Create new island, which has coordinates and guesses.

---
+++ lib/islands_engine.ex
@@ -0,0 +1,19 @@
+defmodule IslandsEngine do
+  @moduledoc """
+  xoxoxo
+  Documentation for IslandsEngine.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> IslandsEngine.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
